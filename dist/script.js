1f1a4f3d6a90f45c17cf1c951aadd32fc6f4ec44
Nidhin Joseph <nidhin.joseph1@gmail.com>
2019-11-24 15:00:47 +1100

    gulp updated to support umd format

--- dist/script.js
+++ dist/script.js
@@ -1,60 +1,73 @@
-var NJEvents = function () {
-    this.events = {};
-    this.listeners = {};
-};
-/**
- * register event
- * @param {string} event
- * @param {function} cb
- * @param {string} id
- */
-NJEvents.prototype.on = function (name, cb, id) {
-    if (typeof (cb) != 'function') {
-        throw ('invalid listener.');
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global.NJEvents = factory());
+}(this, (function () { 'use strict';
+
+class NJEvents {
+    constructor() {
+        this.events = {};
+        this.listeners = {};
     }
-    let evID = id || Math.random().toString(36).substring(7);
-    (this.events[name] = this.events[name] || []).push(evID);
-    this.listeners[evID] = {
-        cb: cb, // call back
-        ev: name // event name
-    };
-    return evID;
-};
-/** 
- * register a one time event 
-*/
-NJEvents.prototype.once = function (event, listener, id) {
-    let key = this.on(event, listener, id);
-    this.listeners[key].once = true;
-};
-/**
- * unregister event
- * @param {string} event 
- * @param {string} evID 
- */
-NJEvents.prototype.off = function (key) {
-    if (this.events[key]) { // check if key matches a event name
-        this.events[key].forEach(ev => {
-            delete this.listeners[ev];
+
+    /**
+     * register event
+     * @param {string} event
+     * @param {function} cb
+     * @param {string} id
+     */
+    on(name, cb, id) {
+        if (typeof (cb) != 'function') {
+            throw ('invalid listener.');
+        }
+        let evID = id || Math.random().toString(36).substring(7);
+        (this.events[name] = this.events[name] || []).push(evID);
+        this.listeners[evID] = {
+            cb: cb, // call back
+            ev: name // event name
+        };
+        return evID;
+    }
+    /** 
+     * register a one time event 
+    */
+    once(event, listener, id) {
+        let key = this.on(event, listener, id);
+        this.listeners[key].once = true;
+    }
+    /**
+     * unregister event
+     * @param {string} event 
+     * @param {string} evID 
+     */
+    off(key) {
+        if (this.events[key]) { // check if key matches a event name
+            this.events[key].forEach(ev => {
+                delete this.listeners[ev];
+            });
+            delete this.events[key];
+        } else if (this.listeners[key]) { // check if key matches a listener name
+            let listener = this.listeners[key];
+            let event = this.events[listener.ev];
+            event.splice(key, 1);
+            delete this.listeners[key];
+        }
+    }
+    /**
+     * emit event
+     * @param {string} event 
+     */
+    emit(name) {
+        var args = [].slice.call(arguments, 1);
+        (this.events[name] || []).forEach(key => {
+            let listener = this.listeners[key];
+            listener.cb(args.length == 1 ? args[0] : args);
+            if (listener.once)
+                this.off(name);
         });
-        delete this.events[key];
-    } else if (this.listeners[key]) { // check if key matches a listener name
-        let listener = this.listeners[key];
-        let event = this.events[listener.ev];
-        event.splice(key, 1);
-        delete this.listeners[key];
     }
-};
-/**
- * emit event
- * @param {string} event 
- */
-NJEvents.prototype.emit = function (name) {
-    var args = [].slice.call(arguments, 1);
-    (this.events[name] || []).forEach(key => {
-        let listener = this.listeners[key];
-        listener.cb(args.length == 1 ? args[0] : args);
-        if (listener.once)
-            this.off(name, ev);
-    });
-};
+}
+
+return NJEvents;
+
+})));
--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,29 @@
 const gulp = require('gulp');
 const rename = require('gulp-rename');
 const minify = require('gulp-minify');
+const rollup = require('gulp-rollup');
+const destFolder = './dist';
 
 // deletes the dist folder
 gulp.task('clean', require('del').bind(null, ['dist']));
 
 // build js
-gulp.task('script', function () {
+gulp.task('build', function () {
     return gulp.src(['./src/*.js'])
-        .pipe(rename('script.js'))
+        .pipe(rollup({
+            format: "umd",
+            moduleName: "NJEvents",
+            entry: "./src/index.js"
+        }))
+        .pipe(rename("script.js"))
         .pipe(minify())
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest(destFolder));
 });
 
 // Create a new build in dist folder
 gulp.task('build', gulp.series(
     'clean',
-    'script'
+    'build'
 ));
 
 // Watch for changes.
